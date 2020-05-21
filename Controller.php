14dcbafe8a5fdbe10917488dafeb3c5f982bda0d
Guilherme Alves <guilherme.alves@jurid.com.br>
2020-05-20 23:53:57 -0300

    init

---
+++ Controller.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace LumenBaseCRUD;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Lumen\Routing\Controller as LumenController;
+
+/**
+ * Classe base para CRUDS com Lumen
+ *
+ * @author Guilherme Alves <guihalves20@gmail.com>
+ */
+class Controller extends LumenController
+{
+    use APIResponse;
+
+    /**
+     * String com o model
+     * Deve ser definida como Model::class
+     *
+     * @var string
+     */
+    protected $model = '';
+
+    /**
+     * Regras de validação no verbo POST
+     *
+     * @var array
+     */
+    protected array $postRules = [];
+
+    /**
+     * Regras de validação no verbo PUT
+     *
+     * @var array
+     */
+    protected array $putRules = [];
+
+    /**
+     * Retorna um objeto de acordo com seu ID
+     *
+     * @param integer $id
+     * @return JsonResponse|void
+     */
+    public function show(int $id): JsonResponse
+    {
+        // Busca o objeto
+        $object = $this->model::find($id);
+
+        if (!$object) {
+            return $this->response(404);
+        }
+
+        $this->preShow($data);
+        return $this->response(200, compact(['data']));
+    }
+
+    /**
+     * Retorna todos os objetos paginados
+     *
+     * @return JsonResponse|void
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $pageSize = config('database.pageSize') ?? 10;
+        $result   = $this->model::paginate($pageSize)->toArray();
+        
+        // Recupero os dados e a paginação
+        $data       = Arr::pluck($result, 'data');
+        $pagination = Arr::except($result, 'data');
+
+        $this->preIndex($data);
+        return $this->response(200, compact(['data', 'pagination']));
+    }
+
+    /**
+     * Salva um objeto
+     *
+     * @param Request $request
+     * @return JsonResponse|void
+     */
+    public function store(Request $request): JsonResponse
+    {
+        // A partir das postRules, valida os dados
+        $data = $request->all();
+        $validator = Validator::make($data, $this->postRules);
+
+        // Se a validação falhar
+        if ($validator->fails()) {
+            $fails = $validator->errors()->all();
+            return $this->response(400, compact('fails'), 'Parâmetros inválidos');
+        }
+
+        $preStoreReturns = $this->preStore($data);
+        if ($preStoreReturns) {
+            return $preStoreReturns;
+        }
+
+        // Efetivamente cria o objeto
+        $created = $this->model::create($data);
+
+        // Se houver algum erro, já retorno
+        if (!$created) {
+            return $this->response(500);
+        }
+
+        // senão, chamo a função posStore e retorno uma resposta de API
+        $this->posStore($created);
+        return $this->response(201, ['id' => $created->id]);
+    }
+
+    /**
+     * Atualiza um objeto
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse|void
+     */
+    public function update(Request $request, int $id): JsonResponse
+    {
+        // Busca o objeto
+        $object = $this->model::find($id);
+
+        // Se o objeto não for encontrado
+        if (!$object) {
+            return $this->response(404);
+        }
+
+        // Valido os dados
+        $data = $request->all();
+        $validator = Validator::make($data, $this->putRules);
+
+        // Se a validação falhar
+        if ($validator->fails()) {
+            $fails = $validator->errors()->all();
+            return $this->response(400, compact('fails'), 'Parâmetros inválidos');
+        }
+
+        $preUpdateReturns = $this->preUpdate($object);
+        if ($preUpdateReturns) {
+            return $preUpdateReturns;
+        }
+
+        $updated = $object->update($data);
+        // Se houver algum erro, já retorno
+        if (!$updated) {
+            return $this->response(500);
+        }
+
+        // senão, chamo a posUpdate e retorno
+        $this->posUpdate($object);
+        return $this->response(200);
+    }
+
+    /**
+     * Deleta um objeto
+     *
+     * @param int $id
+     * @return JsonResponse|void
+     */
+    public function delete(int $id)
+    {
+        $object = $this->model::find($id);
+
+        // Se o objeto não for encontrada
+        if (!$object) {
+            return $this->response(404);
+        }
+
+        $preDeleteReturns = $this->preDelete($object);
+        if ($preDeleteReturns) {
+            return $preDeleteReturns;
+        }
+
+        $object->delete();
+        $this->posDelete($object);
+
+        return $this->response(200);
+    }
+
+    /**
+     * Executada antes de exibição de um objeto
+     *
+     * @param Model $data
+     * @return void
+     */
+    protected function preShow(&$data) {}
+
+    /**
+     * Executada antes de exibição de uma lista de objetos
+     *
+     * @param array $data
+     * @return void
+     */
+    protected function preIndex(array &$data) {}
+
+    /**
+     * Executada antes da criação de um objeto
+     *
+     * @param array $data dados do objeto
+     * @return JsonResponse|void
+     */
+    protected function preStore(array &$data) {}
+
+    /**
+     * Executada após a criação de um objeto
+     *
+     * @param Model $object
+     * @return void
+     */
+    protected function posStore(Model $object) {}
+
+    /**
+     * Executada antes da alteração de um objeto
+     *
+     * @param Model $object
+     * @return JsonResponse|void
+     */
+    protected function preUpdate(Model &$object) {}
+
+    /**
+     * Executada após a alteração de um objeto
+     *
+     * @param Model $object
+     * @return void
+     */
+    protected function posUpdate(Model $object) {}
+
+    /**
+     * Executada antes da deleção de um objeto
+     *
+     * @param Model $object
+     * @return JsonResponse|void
+     */
+    protected function preDelete(Model &$object) {}
+
+    /**
+     * Executada após a deleção de um objeto
+     *
+     * @param Model $object
+     * @return void
+     */
+    protected function posDelete(Model $object) {}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # lumen-base-crud
 The basic CRUD for Lumen MicroFramework
+
+Pacote para servir como base para outros CRUDs em Lumen
+
+---
+## Installation
+Para instalar o pacote, apenas execute: 
+composer require guilhermefalves/lumen-base-crud
+
+---
+## Usage
+É necessário que os Controllers do Lumen extenda a classe Controller deste projeto, ou seja:  
+```
+<?php
+namespace App\Http\Controllers;
+
+use LumenBaseCRUD\Controller as BaseCRUD;
+use App\Models\User;
+
+class UserController extends BaseCRUD
+{
+    protected $model = User::class;
+    // ...
+}
+```
+
+---
+## Customization
+Dentro da classe existem algumas variáveis para a customização, são elas:
+* protected $model = ''; // O model utilizado
+* protected array $postRules = []; // Regras de validação no verbo POST (default: [])
+* protected array $putRules  = []; // Regras de validação no verbo PUT (default: [])
+
+Também existem algumas funções que recebem um ponteiro para os dados e são executadas antes dos respectivos métodos:
+* preShow
+* preIndex
+* preStore
+
+E funções que recebem o objeto e são executadas após dos respectivos métodos:
+* posStore
+* preUpdate
+* posUpdate
+* preDelete
+* posDelete
+
+---
+## Configuration
+O pacote utiliza algumas configurações, são elas:
+* database.pageSize - tamanho de cada página, utilizado nas funções show (default: 10)
